fef4191259e7d3ee4821d76280a9fb26f15253fc
PythonRex <tearexamz@gmail.com>
2023-01-12 12:33:12 +0500

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primaryColor:Color(0xFFBBEA8A),
       ),
-
+// starting from splash
       home: SplashScreen(),
     );
   }
